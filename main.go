6526ae40f5e921a55608e9b9e820de6757fabd5e
SaNsK11 <SaNsK11@users.noreply.github.com>
2022-07-15 16:27:01 +0530

    modification done

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 
-	var conferenceName = "Go conference"
+	conferenceName := "Go conference"
 	const conferenceTickets = 50
 	var remainingTickets = 50
 
@@ -12,7 +12,23 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still remaining.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here")
 
-	var userName string
-	fmt.Println("Welcome", userName)
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
 
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+	fmt.Println("Enter number of tickets you want: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("%v booked %v tickets.\n", firstName, userTickets)
+
+	fmt.Printf("%v tickets are remaining.\n", remainingTickets)
 }
